eb3f157b6e1156a7367ca708dc097f5f1434c255
Sreejanxd🥵 <oyysreejan@gmail.com>
2025-01-11 11:54:03 +0530

    Update Footer.js

--- public/src/components/Layout/Footer.js
+++ public/src/components/Layout/Footer.js
@@ -4,7 +4,7 @@ import gitLogo from "../../assets/icons8-github-logo-48.png";
 const Footer = () => {
   return (
     <footer className={styles["footer"]}>
-      <a href="https://github.com/shuvra-matrix">
+      <a href="https://github.com/itzsreejan">
         <img src={gitLogo} alt="github"></img>
         <span>Shuvra Chakrabarty</span>
       </a>
